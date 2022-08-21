32fc18b5e9485b5b9892076771beec10e7d7f63b
Leslie Lopez <leslie2515lopez@gmail.com>
2022-08-21 18:29:21 -0500

    Added checkboxes to add or subtract value function

---
+++ public/order.js
@@ -0,0 +1,19 @@
+window.menuItems = 0;
+
+const clothingItems = document.getElementsByClassName("clothing");
+
+const clothingTotal = document.getElementById("price");
+
+function callFeeAPI({ target }) {
+  if (target.className === "clothing" && target.checked) {
+    window.menuItems += parseInt(target.value);
+  } else if (target.className === "clothing" && !target.checked) {
+    window.menuItems -= parseInt(target.value);
+  }
+
+  clothingTotal.textContent = `$${(window.menuItems / 100).toFixed(2)}`;
+}
+
+for (const clothing of clothingItems) {
+  clothing.addEventListener("click", callFeeAPI);
+}
