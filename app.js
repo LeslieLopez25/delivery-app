8ca8175805209200e3b7cd35f12bbec20b09cb95
Leslie Lopez <leslie2515lopez@gmail.com>
2022-08-21 17:50:03 -0500

    Added getting delivery quote function

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 import path from "path";
 import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from "uuid";
+import "dotenv/config";
+import { DoorDashClient } from "@doordash/sdk";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,4 +19,22 @@ app.listen(port, (err) => {
     return console.log("ERROR", err);
   }
   console.log(`App listening on port ${port}`);
+});
+
+app.post("/get-delivery-rate", async (req, res) => {
+  const client = new DoorDashClient({
+    developer_id: process.env.DEVELOPER_ID,
+    key_id: process.env.KEY_ID,
+    signing_secret: process.env.SIGNING_SECRET,
+  });
+
+  const response = await client.deliveryQuote({
+    external_delivery_id: uuidv4(),
+    pickup_address: "1000 4th Ave, Seattle, WA, 98104",
+    pickup_phone_number: "+1(650)5555555",
+    dropoff_address: "1201 3rd Ave, Seattle, WA, 98101",
+    dropoff_phone_number: "+1(650)5555555",
+  });
+
+  res.send(response);
 });
